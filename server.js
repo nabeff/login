5ec2e07adbefd82e5cc5623b3bbe053975311382
nabeff <neffina8@gmail.com>
2024-01-03 00:35:03 +0100

    creating user

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const User = require("./model/user");
+const bcrypt = require("bcrypt");
 
-mongoose.connect("mongodb://localhost:27017/login-app-db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://localhost:27017/login-app-db");
+
 const app = express();
+
 app.use("/", express.static(path.join(__dirname, "static")));
 app.use(bodyParser.json());
 
 app.post("/api/register", async (req, res) => {
   console.log(req.body);
+
+  const { username, password: plainTextPassword } = req.body;
+
+  const password = await bcrypt.hash(password, 10);
+
   res.json({ status: "ok" });
 });
 

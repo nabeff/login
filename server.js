30f6a1c1cde52ea90481d29301eda76cb4549576
nabeff <neffina8@gmail.com>
2024-01-03 00:18:46 +0100

    getting data on server

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const User = require("./model/user");
 
+mongoose.connect("mongodb://localhost:27017/login-app-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const app = express();
 app.use("/", express.static(path.join(__dirname, "static")));
+app.use(bodyParser.json());
+
+app.post("/api/register", async (req, res) => {
+  console.log(req.body);
+  res.json({ status: "ok" });
+});
 
 app.listen(9999, () => {
   console.log("Server up at 9999");
